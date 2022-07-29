d2a1a076dd7ce87777e8a67b4cb2540f5f4ab635
Tehseen <47675253+MdTehseenKhan@users.noreply.github.com>
2022-07-29 19:05:52 +0500

    HeroBanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -2,23 +2,39 @@ import HeroBannerImage from "../assets/images/banner.png"
 
 const HeroBanner = () => {
   return (
-    <div>
-      Fitness Craze Sweat, Smile <br /> & Repeat. Check out the most effective
-      exercises
-      <a href="#exercises">Explore Exercises</a>
-      <div
-        className="opacity-10"
-        // sx={{
-        //   opacity: 0.1,
-        //   display: { lg: "block", xs: "none" },
-        //   fontWeight: 600,
-        //   fontSize: 200,
-        // }}
-      >
-        Exercises
-      </div>
-      <img src={HeroBannerImage} alt="" className="hero-banner-img" />
-    </div>
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Fitness Craze Sweat, Smile
+              <br className="inline-block md:hidden" /> & Repeat.
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Check out the most effective exercises
+            </p>
+            <div className="flex justify-center">
+              <a
+                href="#exercises"
+                className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              >
+                Explore Exercises
+              </a>
+            </div>
+            <div className="opacity-10 hidden sm:block font-semibold text-[200px] text-red-600 -z-10">
+              Exercises
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              src={HeroBannerImage}
+              alt="Banner"
+              className="object-cover object-center rounded"
+            />
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,30 @@ import Logo from "../assets/images/Logo.png"
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">
-        <img
-          src={Logo}
-          alt="Fitness Craze"
-          style={{ width: "48px", height: "48px", margin: "0 20px" }}
-        />
-      </Link>
-      <div>
-        <button>
-          <Link to="/" style={{ textDecoration: "none" }}>
-            Home
-          </Link>
-        </button>
-        <a href="#exercises">Exercises</a>
-      </div>
-    </div>
+    <>
+      <header className="text-gray-600 body-font">
+        <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <Link to="/">
+              <img
+                src={Logo}
+                alt="Fitness Craze"
+                className="w-6, h-6 mx-0 my-5"
+              />
+            </Link>
+            <span className="ml-3 text-2xl">Fitness Freak</span>
+          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link to="/" className="mr-5 hover:text-indigo-900">
+              Home
+            </Link>
+          </nav>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Exercises
+          </button>
+        </div>
+      </header>
+    </>
   )
 }
 
